de9d2a6b1c8d49ff25c8e49337cfaabb8eff1b86
Alena <alena@MacBook-Air-Alena.local>
2019-09-10 13:38:53 +0300

    31 hook work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
+
 import BigList from './components/BigList'
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, end: false, title: 'buy milk' },
     { id: 2, end: false, title: 'feed the cat' },
     { id: 3, end: false, title: 'to water flowers' },
     { id: 4, end: false, title: 'check mail' }
-  ]
+  ])
 
   let togglesTodo = id => {
-    todos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.end = !todo.end
-      }
-      return todo
-    })
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.end = !todo.end
+        }
+        return todo
+      })
+    )
   }
 
   return (
